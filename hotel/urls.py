9d3a186d08db91a59c0098e9b60f892e7464395a
ravion223 <tablettaoson2016@gmail.com>
2024-03-02 13:20:02 +0200

    finished second classwork

--- hotel/urls.py
+++ hotel/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', get_rooms_list),
-    path('users/', get_users_list),
+    path('', get_rooms_list, name = "rooms_list"),
+    path('users/', get_users_list, name = "users_list"),
+    path('room/<int:pk>', get_room_detail, name = "room_detail"),
+    path('booking/', booking_form, name = "booking_form"),
+    path('booking_detail/<int:pk>', get_booking_detail, name = "booking_detail")
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from hotel.models import *
 
 # Create your views here.
@@ -16,6 +17,7 @@ def get_rooms_list(request):
         context,
     )
 
+
 def get_users_list(request):
     users = User.objects.all()
 
@@ -27,4 +29,59 @@ def get_users_list(request):
         request,
         "hotel/users_list.html",
         context,
+    )
+
+
+def get_room_detail(request, pk: int):
+    room = Room.objects.get(id = pk)
+
+    context = {
+        "room": room
+    }
+
+    return render(
+        request,
+        "hotel/room_detail.html",
+        context
+    )
+
+
+def booking_form(request):
+    if request.method == "GET":
+        return render(request, "hotel/booking_form.html")
+    else:
+        room_number = request.POST.get("room_number")
+        start_time = request.POST.get("start_time")
+        end_time = request.POST.get("end_time")
+
+        try:
+            room = Room.objects.get(number=room_number)
+
+        except ValueError:
+            return HttpResponse("Wrong room number", status=404)
+        
+        except Room.DoesNotExist:
+            return HttpResponse("Room does not exist", status=404)
+        
+        booking = Booking.objects.create(
+            room = room,
+            user = request.user,
+            start_time =  start_time,
+            end_time = end_time
+        )
+
+        return redirect("booking_detail", pk = booking.id)
+
+
+def get_booking_detail(request, pk: int):
+    booking = Booking.objects.get(id = pk)
+
+    context = {
+        "booking": booking
+    }
+
+    return render(
+        request,
+        "hotel/booking_detail.html",
+        context
     )
